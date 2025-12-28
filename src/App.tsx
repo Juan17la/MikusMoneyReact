c468bcb22b9cef1998f2fe342331699edcb210b1
Jud1714 <juandiegolopezarias07@gmail.cm>
2025-12-27 20:52:42 -0500

    Setting up and refactor to make funcional Public and Private routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import { Routes, Route } from 'react-router'
-import Home from './pages/Home.tsx'
+import Account from './pages/Account.tsx'
+import Login from './pages/Login.tsx'
+import Error404 from './pages/404.tsx'
+
+import { PrivateRoute } from './Routes/PrivateRoute.tsx'
+import { PublicOnlyRoute } from './Routes/PublicOnlyRoute.tsx'
 
 function App() {
   return (
    <>
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/login' element={<h1>Login HIIII</h1>} />
-      <Route path='/register' element={<h1>Register please, dummy!</h1>} />
+      {/* RUTAS PUBLICAS */}
+      <Route path='/login' element={<PublicOnlyRoute><Login /></PublicOnlyRoute>} />
+      <Route path='/register' element={<PublicOnlyRoute><h1>Register please, dummy!</h1></PublicOnlyRoute>} />
+
+      {/* RUTAS PRIVADAS */}
+      <Route path='/account' element={<PrivateRoute><Account /></PrivateRoute>} />
+      
+      {/* RUTAS LANDING */}
+      <Route path='/' element={<h1>Welcome to Mikus Money!</h1>} />
+      
+      {/* ERROR 404 NOT FOUND */}
+      <Route path='*' element={<Error404 />} />
     </Routes>
    </> 
   )
