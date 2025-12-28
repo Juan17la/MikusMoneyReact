0f3a3056b8f69447125a72949726138c9209ca41
Jud1714 <juandiegolopezarias07@gmail.com>
2025-12-28 15:43:26 -0500

    Adding register function with anxios instance for the registration process

--- src/api/authenticationService.ts
+++ src/api/authenticationService.ts
@@ -8,17 +8,51 @@ export async function login(email:string, pin:string) {
     }
 
     const response = await axiosInstance.post('/auth/login', requestBody);
-    return { response, message: 'Login successful' };
+    return { data: response.data, message: 'Login successful' };
   } catch (error) {
-    return console.error('Axios error:', error);
+    console.error('Axios error:', error);
+    return {data: null, message: 'Login failed'}; 
+  }
+}
+
+export async function register(
+  name: string,
+  lastName: string,
+  birthDate: string,
+  email: string,
+  phoneNumber: string,
+  pin: string,
+  pinConfirmation: string,
+  password: string,
+  passwordConfirmation: string
+) {
+  try {
+    const requestBody = {
+      name,
+      lastName,
+      birthDate,
+      email,
+      phoneNumber,
+      pinCode: pin,
+      pinCodeConfirmation: pinConfirmation,
+      password,
+      passwordConfirmation
+    }
+
+    const response = await axiosInstance.post('/auth/register', requestBody);
+    return { data: response.data, message: 'Register successful' };
+  } catch (error: any) {
+    console.error('Axios error:', error);
+    throw new Error(error.response?.data?.message || 'Register failed');
   }
 }
 
 export async function logout() {
   try {
     const response = await axiosInstance.post('/auth/logout');
-    return { response, message: 'Logout successful' };
+    return { data: response.data, message: 'Logout successful' };
   } catch (error) {
-    return console.error('Axios error:', error);
+    console.error('Axios error:', error);
+    return {data: null, message: 'Logout failed'}; 
   }
 }
