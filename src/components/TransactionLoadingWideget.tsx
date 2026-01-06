72d675418a92bb9e1bcc2513324381abc0a28d09
Jud1714 <juandiegolopezarias07@gmail.com>
2026-01-06 10:18:30 -0500

    Add TransactionsLoadingWidget component for displaying loading state with a visual indicator

---
+++ src/components/TransactionLoadingWideget.tsx
@@ -0,0 +1,19 @@
+import mikuNotfoundSticker from "../assets/mikusNotFound.png";
+
+export default function TransactionsLoadingWidget() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-w-full min-h-full py-10 px-8">
+        <img
+          src={mikuNotfoundSticker}
+          alt="sticker here"
+          className="w-25 h-25 mb-4"
+          loading="lazy"
+        />
+        <p className="text-white/60 text-center">
+          Loading transactions...
+        </p>
+      </div>
+    </>
+  );
+}
