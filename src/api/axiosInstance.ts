0efe3a9404e6f2b051796a8e97882aeb3bdf3c96
Jud1714 <juandiegolopezarias07@gmail.com>
2026-01-18 18:54:25 -0500

    Update axios instance base URL to production endpoint

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:10101/api/v0.1',
+  baseURL: 'https://mikus-money-alpha.onrender.com/api/',
   withCredentials: true,
 });
 
