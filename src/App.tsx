8c6814eccfe03c8f3151e8f823132cb4d82c62c6
Jud1714 <juandiegolopezarias07@gmail.com>
2026-01-18 18:24:34 -0500

    Refactor routing structure in App component for better organization and clarity

--- src/App.tsx
+++ src/App.tsx
@@ -9,26 +9,38 @@ import Error404 from './pages/404.tsx'
 
 import DepositForm from './pages/DepositForm.tsx'
 import WithdrawForm from './pages/WithdrawForm.tsx'
+import TransferForm from './pages/TransferForm.tsx'
+import Transactions from './pages/Transactions.tsx'
+import Savings from './pages/Savings.tsx'
+import SavingsCreateForm from './pages/SavingsCreateForm.tsx'
+import DepositSavingsForm from './pages/DepositSavingsForm.tsx'
 
 import { PrivateRoute } from './Routes/PrivateRoute.tsx'
 import { PublicOnlyRoute } from './Routes/PublicOnlyRoute.tsx'
-import TransferForm from './pages/transferForm.tsx'
 
 function App() {
   return (
    <>
     <Routes>
-      {/* RUTAS PUBLICAS */}
-      <Route path='/login' element={<PublicOnlyRoute><Login /></PublicOnlyRoute>} />
-      <Route path='/register' element={<PublicOnlyRoute><Register /></PublicOnlyRoute>} />
+      {/* AUTH ROUTES */}
+      <Route path='/auth/login' element={<PublicOnlyRoute><Login /></PublicOnlyRoute>} />
+      <Route path='/auth/register' element={<PublicOnlyRoute><Register /></PublicOnlyRoute>} />
 
-      {/* RUTAS PRIVADAS */}
+      {/* PRIVATE ROUTES */}
       <Route path='/account' element={<PrivateRoute><Account /></PrivateRoute>} />
-      <Route path='/deposit' element={<PrivateRoute><DepositForm /></PrivateRoute>} />
-      <Route path='/withdraw' element={<PrivateRoute><WithdrawForm /></PrivateRoute>} />
-      <Route path='/transfer' element={<PrivateRoute><TransferForm /></PrivateRoute>} />
+
+      {/* TRANSACTIONS ROUTES */}
+      <Route path='/transactions' element={<PrivateRoute><Transactions /></PrivateRoute>} />
+      <Route path='/transactions/deposit' element={<PrivateRoute><DepositForm /></PrivateRoute>} />
+      <Route path='/transactions/withdraw' element={<PrivateRoute><WithdrawForm /></PrivateRoute>} />
+      <Route path='/transactions/transfer' element={<PrivateRoute><TransferForm /></PrivateRoute>} />
+
+      {/* SAVINGS ROUTES */}
+      <Route path='/savings' element={<PrivateRoute><Savings /></PrivateRoute>} />
+      <Route path='/savings/create' element={<PrivateRoute><SavingsCreateForm /></PrivateRoute>} />
+      <Route path='/savings/:id/deposit' element={<PrivateRoute><DepositSavingsForm /></PrivateRoute>} />
       
-      {/* RUTAS LANDING */}
+      {/* LANDING */}
       <Route path='/' element={<PublicOnlyRoute><h1>Welcome to Mikus Money!</h1></PublicOnlyRoute>} />
       
       {/* ERROR 404 NOT FOUND */}
